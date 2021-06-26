2dde98a32afcea6cffe1b87924b4af6c36882ae7
Sidinei Silva <sidinei.silva02@gmail.com>
2021-06-26 20:24:20 -0300

    Criando a extractPage

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/extract/extract_page.dart';
 import 'package:payflow/modules/home/home_controller.dart';
 import 'package:payflow/modules/meus_boletos/meus_boletos_page.dart';
 import 'package:payflow/shared/models/boleto_model.dart';
@@ -17,9 +18,7 @@ class _HomePageState extends State<HomePage> {
 
   final pages = [
     MeusBoletosPage(),
-    Container(
-      color: Colors.blue,
-    ),
+    ExtractPage(),
   ];
 
   @override
@@ -74,7 +73,9 @@ class _HomePageState extends State<HomePage> {
               icon: Icon(
                 Icons.home,
               ),
-              color: AppColors.primary,
+              color: controller.currentPage == 0
+                  ? AppColors.primary
+                  : AppColors.body,
             ),
             InkWell(
               onTap: () {
@@ -100,7 +101,9 @@ class _HomePageState extends State<HomePage> {
               },
               icon: Icon(
                 Icons.description_outlined,
-                color: AppColors.body,
+                color: controller.currentPage == 1
+                    ? AppColors.primary
+                    : AppColors.body,
               ),
             )
           ],
